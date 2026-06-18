83dfa537dd397916a552463891d240a79ed5d0b8
Luca Foglia <luca.foglia@edu-its.it>
2026-06-19 01:37:27 +0200

    feat(android): RiepilogoView dispone le statistiche 2x2 su touch

--- wsa.quiz.app/Views/RiepilogoView.axaml.cs
+++ wsa.quiz.app/Views/RiepilogoView.axaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Media;
@@ -46,6 +47,32 @@ public partial class RiepilogoView : UserControl, INotifyPropertyChanged
     public RiepilogoView(RisultatoQuiz risultato)
     {
         InitializeComponent();
+
+        if (AppEnv.TouchMode)
+        {
+            // 5 colonne -> percentuale a tutta riga in alto, metriche 2x2 sotto.
+            var g = GrigliaStat;
+            g.ColumnDefinitions = new ColumnDefinitions("*,*");
+            g.RowDefinitions = new RowDefinitions("Auto,Auto,Auto,Auto");
+
+            // Percentuale (era Col0 RowSpan2): tutta la prima riga.
+            Grid.SetColumn(BloccoPercentuale, 0);
+            Grid.SetColumnSpan(BloccoPercentuale, 2);
+            Grid.SetRow(BloccoPercentuale, 0);
+            Grid.SetRowSpan(BloccoPercentuale, 1);
+
+            // 4 metriche (Totale/Corrette/Errate/Punteggio) in griglia 2x2, righe 1-2.
+            PosizionaMetrica(BloccoTotale,    col: 0, row: 1);
+            PosizionaMetrica(BloccoCorrette,  col: 1, row: 1);
+            PosizionaMetrica(BloccoErrate,    col: 0, row: 2);
+            PosizionaMetrica(BloccoPunteggio, col: 1, row: 2);
+
+            // Riga durata/modalità: ultima riga, due colonne.
+            Grid.SetColumn(BloccoDurataModalita, 0);
+            Grid.SetColumnSpan(BloccoDurataModalita, 2);
+            Grid.SetRow(BloccoDurataModalita, 3);
+        }
+
         _risultato = risultato;
 
         Titolo = risultato.Abbandonato ? "Quiz abbandonato"
@@ -71,5 +98,13 @@ public partial class RiepilogoView : UserControl, INotifyPropertyChanged
     private void OnTornaClick(object? sender, RoutedEventArgs e)
     {
         TornaAllaHome?.Invoke(this, EventArgs.Empty);
+    }
+
+    static void PosizionaMetrica(Control c, int col, int row)
+    {
+        Grid.SetColumn(c, col);
+        Grid.SetColumnSpan(c, 1);
+        Grid.SetRow(c, row);
+        c.Margin = new Thickness(0, 8, 0, 0);
     }
 }
